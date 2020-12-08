0d7a0f33d68449558cd37968d0140d52aaf9409b
Fiona Morris <morri22f@gmail.com>
2020-12-08 15:21:03 -0500

    Program Set Up
    Setting up initial files and starting to create div elements with javascript.

---
+++ script.js
@@ -0,0 +1,6 @@
+let gridItem = document.createElement("div");
+gridItem.style.width = "30px";
+gridItem.style.height = "30px";
+gridItem.style.background = "red";
+
+document.body.appendChild(gridItem);
